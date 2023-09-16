11c7b23220eb00179ab7181a5e24d1dd2371c177
Raymondkingjnr <raymondnnaji660@gmail.com>
2023-09-16 10:51:49 +0100

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import logo from "../images/tv.png";
+import menu from "../images/Menu.png";
 
 const Navbar = ({ search, setSearch, searchMovies }) => {
   return (
@@ -26,6 +27,7 @@ const Navbar = ({ search, setSearch, searchMovies }) => {
 
         <div className="sign">
           <p>Sign in</p>
+          <img src={menu} alt="menu" />
         </div>
       </div>
       <div className="input_small_screen">
